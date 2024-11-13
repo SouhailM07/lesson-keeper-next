563679a33248a5b58b1adc8f4998292a263a383d
souhail <lightshadow416@gmail.com>
2024-11-13 11:50:11 +0100

    adding refs to user->seasons->modules

--- src/models/season.model.ts
+++ src/models/season.model.ts
@@ -3,12 +3,13 @@ import { Model, model, Schema, Document } from "mongoose";
 interface ISeason extends Document {
   name: string;
   duration?: any;
-  modules?: any;
+  userBy: any;
 }
 
 const seasonSchema = new Schema<ISeason>({
   name: { type: String, required: true },
   duration: { type: Schema.Types.Mixed, default: null },
+  userBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 let Season: Model<ISeason>;
